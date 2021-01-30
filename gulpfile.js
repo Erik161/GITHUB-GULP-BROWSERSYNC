3bc95c0d200f4ca11ba78379266692edc63cadbb
Erik Hernandez <erikestuardo_161@hotmail.com>
2021-01-29 21:43:07 -0600

    add space

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,52 @@
-const gulp = required ('gulp');
-const browserSync = require('browser-sync');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
-gulp.task('sass', ()=>{
-return gulp.src([
+gulp.task('sass', () => {
+  return gulp.src([
     'node_modules/bootstrap/scss/bootstrap.scss',
     'src/scss/*.scss'
- ])
- .pipe(sass({outputStyle:'compressed'}))
- .pipe(gulp.dest('src/css'))
- .pipe(browserSync.stream());
+  ])
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.stream());
 });
 
-
-gulp.task('js', ()=>{
-return gulp.src([
+gulp.task('js', () => {
+  return gulp.src([
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popper.js/dist/amd/popper.min.js'
-])
-    .pipe(gulp.dest('src/js'))
-    .pipe(browserSync.stream());
+    'node_modules/popper.js/dist/umd/popper.min.js'
+  ])
+  .pipe(gulp.dest('src/js'))
+  .pipe(browserSync.stream());
 });
 
+gulp.task('serve', ['sass'], () => {
+  browserSync.init({
+    server: './src'
+  });
 
-
-gulp.task('serve',['sass'],() =>{
-browserSync.init({
-    server:'./src'
-});
-
-gulp.watch([
-    'node_modules/bootstrap/scss/bootstrap.scss',
+  gulp.watch([
+    'node_modules/bootstrap/scss/bootstrap.min.scss',
     'src/scss/*.scss'
-],['sass']);
+  ], ['sass']);
 
-gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+
+});
+
+gulp.task('font-awesome', () => {
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+  .pipe(gulp.dest('src/css'));
 })
 
-gulp.task('font-awesome', () =>{
- return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
- .pipe(gulp.dest('src/css'));
-});
-
-
-gulp.task('fonts',() =>{
-    return gulp.src('node_modules/font-awesome/fonts/*')
+gulp.task('fonts', () => {
+  return gulp.src('node_modules/font-awesome/fonts/*')
     .pipe(gulp.dest('src/fonts'));
 });
 
-gulp.task('default',['js','serve','font-awesome','fonts']);
-
-
-
-
-
-
-
-
-
-
-
+gulp.task('default', ['js', 'serve', 'font-awesome', 'fonts'])
 
 
 
